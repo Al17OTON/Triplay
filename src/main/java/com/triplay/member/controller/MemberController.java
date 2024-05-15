9d987ad236571f3f7025af6244db09417723a1d9
limkwon <limkwon18@gmail.com>
2024-05-16 08:58:32 +0900

    update : JWT init

--- src/main/java/com/triplay/member/controller/MemberController.java
+++ src/main/java/com/triplay/member/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.triplay.member.service.MemberService;
 
 @RestController
-@RequestMapping("/api/member")
+@RequestMapping("/member")
 @CrossOrigin(origins = {"*"}, maxAge = 60000)
 public class MemberController {
 	@Autowired
