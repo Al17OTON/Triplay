b87c315a2536cb7f379c6894f7af6a9b8499cbf5
임권 <limkwon18@gmail.com>
2024-05-15 12:40:40 +0900

    init

---
+++ src/test/java/com/triplay/TriplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
