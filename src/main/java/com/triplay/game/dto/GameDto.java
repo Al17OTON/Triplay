c7ccf11b8736d9ac0befef5a09aa3a019ade5a4a
yobinmok <dyqls1214@gmail.com>
2024-05-17 00:08:43 +0900

    feat: gameMapper 메서드 수정(join)

--- src/main/java/com/triplay/game/dto/GameDto.java
+++ src/main/java/com/triplay/game/dto/GameDto.java
@@ -17,4 +17,10 @@ public class GameDto {
     private String memberId;
     private String difficulty;
     private String gameTitle;
+    private String keyword;
+    private String count;
+    private String seedInfo;
 }
+
+//select s.keyword, s.count, s.seed_info, g.member_id, g.difficulty, game_title
+//from seed s join game g on s.seed_id = g.seed_id;
